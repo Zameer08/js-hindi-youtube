9f593aad4731832c149f7316bdd264d7b3be1f06
Zameer08 <155325052+Zameer08@users.noreply.github.com>
2024-09-18 12:23:50 +0000

    Learnt datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser. we can also use the console of the brower enginer it will give answer of it instead of it because js engine is tied to browsr engines
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "Hashira"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+/* Remember this that the type of undefined is undefined which is a type itself in it but null is showed or appearead as an object on the terminal, there is a lot of debate which says it is an error in the language and some dont oppose it at all and reply that it is a part of the language that null should be an object*/
